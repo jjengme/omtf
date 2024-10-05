0e112674d09d48ba16648dcf86069d0c24f1bb1b
정민 <v02happy@naver.com>
2024-10-05 17:52:05 +0900

    wic, clahe

--- cine_wia.py
+++ cine_wia.py
@@ -27,7 +27,7 @@ cn = 'wi2.cine'   #cn : cine 파일 이름
 #     return wi_a
     
 average_image =  avg_cine(file_path)
-wi_a = average_image/np.max(average_image)
+whimg = average_image/np.max(average_image)
 
 # 원본 이미지와 white average 이미지 비교
 # fname = 'Z:/03 exp/240905/wi2.cine'
@@ -35,6 +35,32 @@ wi_a = average_image/np.max(average_image)
 # raw = cine2nparr(fname, frame)
 
 # cv.imshow('raw',raw)
-# cv.imshow('wi_a',wi_a)
+# cv.imshow('whimg',whimg)
 # cv.waitKey()
 # cv.destroyAllWindows() 
+
+# 크롭하기 위한 좌표
+nys = 80
+nye = 1170
+nxs = 0
+nxe = 360
+
+whimg = whimg[nys:nye, nxs:nxe]
+
+path = 'Z:/03 exp/240905/case5.cine'
+frame = 0
+
+raw = cine2nparr(path, frame)
+raw = raw[nys:nye, nxs:nxe]
+
+#  white image correction
+wic = np.array(np.round(raw/whimg), dtype=np.uint8) 
+wic = cv.normalize(wic, None, 0, 255, cv.NORM_MINMAX) # stretching
+
+# clahe 객체 정의
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+# 2. 명암비 강화
+he = cv.equalizeHist(wic) # 2-1 히스토그램 평활화
+cl = clahe.apply(wic) # 2-2 CLAHE
+la = wic - 0.5 * cv.Laplacian(wic, cv.CV_16S, 3) # 2-3 laplacian sharpening
+la = cv.normalize(la, None, 0, 255, cv.NORM_MINMAX)
